c17af31566cceae8ebe3769858ea73da6242c4cc
JEON-Sungsu-Monki <ssjeon@monki.net>
2026-01-29 21:58:06 +0900

    feat: font 적용 및 SafeArewProvider 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,39 @@
 import queryClient from '@/lib/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'Pretendard-Regular': require('@/assets/fonts/Pretendard-Regular.ttf'),
+    'Pretendard-Medium': require('@/assets/fonts/Pretendard-Medium.ttf'),
+    'Pretendard-SemiBold': require('@/assets/fonts/Pretendard-SemiBold.ttf'),
+    'Pretendard-Bold': require('@/assets/fonts/Pretendard-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RootNavigator />
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }} edges={['left', 'right']}>
+          <RootNavigator />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 }
